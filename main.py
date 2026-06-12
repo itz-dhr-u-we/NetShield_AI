e7e7807c06b55c7c7379c6dbb191839585669d54
Dhruvi Ladvaiya <23cs01035@iitbbs.ac.in>
2026-06-12 11:46:14 +0530

    Model training completed with MLFlow

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
 
+
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainerConfig
 import sys
 
 
@@ -32,6 +35,11 @@ if __name__ == "__main__":
         print(data_transformation_artifact)
         logging.info("Data Transformation ended.")
 
+        logging.info("Model Training started")
+        model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact= model_trainer.initiate_model_trainer()
+        logging.info("Model Training artifact created")
 
 
     except Exception as e:
