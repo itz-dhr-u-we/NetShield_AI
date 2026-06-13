f87e671e041b39a8d45062155ef5ee5259a2bce6
Dhruvi Ladvaiya <23cs01035@iitbbs.ac.in>
2026-06-13 14:40:24 +0530

    few changes for render deployment

--- app.py
+++ app.py
@@ -128,4 +128,9 @@ def get_metrics():
         }
 
 if __name__=="__main__":
-    app_run(app,host="0.0.0.0",port=8000)
+    import uvicorn
+
+    port = int(os.environ.get("PORT", 8000))
+
+    uvicorn.run(app, host="0.0.0.0", port=port)
+    
